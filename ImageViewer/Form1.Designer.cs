52b43f2b477967d9c758be0b02324037e0769022
LSLIM <LSLIM@LS>
2018-02-10 20:46:10 +0100

    ImageViewer is complete

--- ImageViewer/Form1.Designer.cs
+++ ImageViewer/Form1.Designer.cs
@@ -29,11 +29,61 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listViewFile = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(548, 320);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "if_jpeg_47107.png");
+            // 
+            // listViewFile
+            // 
+            this.listViewFile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewFile.CheckBoxes = true;
+            this.listViewFile.LargeImageList = this.imageList1;
+            this.listViewFile.Location = new System.Drawing.Point(12, 12);
+            this.listViewFile.Name = "listViewFile";
+            this.listViewFile.Size = new System.Drawing.Size(622, 302);
+            this.listViewFile.TabIndex = 1;
+            this.listViewFile.UseCompatibleStateImageBehavior = false;
+            this.listViewFile.ItemActivate += new System.EventHandler(this.listViewFile_ItemActivate);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 352);
+            this.Controls.Add(this.listViewFile);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listViewFile;
     }
 }
 
